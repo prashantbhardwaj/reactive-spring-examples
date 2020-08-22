84097aa9c486584224e17ef9d97ded89a387a2c6
Prashant Bhardwaj <amu.prashant@gmail.com>
2020-08-22 02:05:22 +0100

    inserting and reading data into/from mongo db

---
+++ src/main/java/person/prashant/react/spring/demo/DemoApplication.java
@@ -0,0 +1,62 @@
+package person.prashant.react.spring.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
+@Component
+@RequiredArgsConstructor
+@Log4j2
+class SampleDataInitializer {
+	private final ReservationRepository reservationRepository;
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void ready(){
+		Flux<Reservation> reservations = Flux
+				.just("Prashant", "Anu", "Dhanya", "Bhaargav", "Ashok", "Asha", "Shweta", "Divya")
+				.map(name -> new Reservation(null, name))
+				.flatMap(res -> this.reservationRepository.save(res));
+		//.subscribe(log::info);
+		this.reservationRepository
+				.deleteAll()
+				.thenMany(reservations)
+				//.thenMany(this.reservationRepository.findAll())
+				.subscribe(log::info);
+
+	}
+
+}
+
+interface ReservationRepository extends ReactiveCrudRepository<Reservation, String>{
+	Flux<Reservation> findByName(String name);
+}
+
+@Document
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Reservation {
+	@Id
+	private String id;
+	private String name;
+}
---
+++ src/test/java/person/prashant/react/spring/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package person.prashant.react.spring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
