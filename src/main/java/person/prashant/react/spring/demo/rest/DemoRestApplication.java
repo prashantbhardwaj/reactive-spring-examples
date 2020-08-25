7213f5fcf22850ac26bde6825f8a75495a163380
Prashant Bhardwaj <amu.prashant@gmail.com>
2020-08-26 00:52:07 +0100

    Return a flux of events over Rest

--- src/main/java/person/prashant/react/spring/demo/rest/DemoRestApplication.java
+++ src/main/java/person/prashant/react/spring/demo/rest/DemoRestApplication.java
@@ -7,15 +7,21 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
 
+import java.time.Duration;
 import java.time.Instant;
+import java.util.stream.Stream;
 
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
@@ -29,7 +35,8 @@ public class DemoRestApplication {
     @Bean
     RouterFunction<ServerResponse> routes(GreetingService gs){
         return route()
-                .GET("/functional/greeting/{name}", serverRequest -> ServerResponse.ok().body(gs.greet(new GreetingRequest(serverRequest.pathVariable("name"))), GreetingResponse.class))
+                .GET("/functional/greeting/{name}", serverRequest -> ServerResponse.ok().body(gs.greetOnce(new GreetingRequest(serverRequest.pathVariable("name"))), GreetingResponse.class))
+                .GET("/functional/greetings/{name}", serverRequest -> ServerResponse.ok().contentType(MediaType.TEXT_EVENT_STREAM).body(gs.keepGreeting(new GreetingRequest(serverRequest.pathVariable("name"))), GreetingResponse.class))
                 .build();
     }
 
@@ -42,7 +49,7 @@ class GreetingRestController {
 
     @GetMapping("/mvc/greeting/{name}")
     Mono<GreetingResponse> greet(@PathVariable String name){
-        return this.gs.greet(new GreetingRequest(name));
+        return this.gs.greetOnce(new GreetingRequest(name));
     }
 }
 
@@ -52,8 +59,15 @@ class GreetingService {
         return new GreetingResponse("Hello " + name + " @" + Instant.now());
     }
 
-    public Mono<GreetingResponse> greet(GreetingRequest request){
+    public Mono<GreetingResponse> greetOnce(GreetingRequest request){
         return Mono.just(greet(request.getName()));
+    }
+
+    public Flux<GreetingResponse> keepGreeting(GreetingRequest request){
+        return Flux
+                .fromStream(Stream.generate(() -> greet(request.getName())))
+                .delayElements(Duration.ofSeconds(1));
+                //.subscribeOn(Schedulers.elastic());
     }
 }
 
